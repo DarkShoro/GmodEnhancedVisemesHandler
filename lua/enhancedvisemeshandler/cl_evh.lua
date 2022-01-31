58aa394206f926835680cdfd0a43f62d07f82dc1
Virtualraptor <chazhumphries9@gmail.com>
2022-01-31 07:08:56 -0800

    Loading test
    Organizing hook load order to make sure localplayer exists when running client side code.

--- lua/enhancedvisemeshandler/cl_evh.lua
+++ lua/enhancedvisemeshandler/cl_evh.lua
@@ -1,10 +1,9 @@
-
-Era_ntstr = "Eradium.MouthMove.Update"
-
 --Added that back into client to avoid error
 function Era_hAdd(hok, func)
 	hook.Add(hok, "Eradium.MouthMove.Hook." .. hok, func)
 end
+print("Era_loaded",Era_loaded)
+Era_loaded = Era_loaded or false
 
 -- we use this to send an update to the server if we are talking or not
 function updateSpeak(isSpeak)
@@ -13,33 +12,49 @@ function updateSpeak(isSpeak)
 		net.Start(Era_ntstr)
 		net.WriteBool(false)
 		net.SendToServer()
+		print("Speaking stop")
 	elseif isSpeak then
-	-- if we are talking then send that we are talking
-	-- and also send our voice volume
+		-- if we are talking then send that we are talking
+		-- and also send our voice volume
 		net.Start(Era_ntstr)
 		net.WriteBool(true)
+		local vol = LocalPlayer():VoiceVolume()
 		net.WriteFloat(LocalPlayer():VoiceVolume())
 		net.SendToServer()
-		Era_mouthMove(LocalPlayer())
+		print("volume client", vol)
+		-- Era_mouthMove(LocalPlayer())
 	end
 end
 
-local speaking = false
--- adding them to a list of speaking players, don't think bots can speak so no need to check
-Era_hAdd("PlayerStartVoice", function(ply)
-	speaking = true
-end)
+local speaking
+function Era_LoadCL()
+	Era_loaded = true
+	print("testLoad", string.rep("testasd", 10, "\n"))
 
--- removing them to a list of speaking players
-Era_hAdd("PlayerEndVoice", function(ply)
-	updateSpeak(false)
-	speaking = nil
-end)
+	-- adding them to a list of speaking players, don't think bots can speak so no need to check
+	Era_hAdd("PlayerStartVoice", function(ply)
+		speaking = true
+	end)
 
--- Create a timer clientside to update the server on our voice volume
--- we also call this every .01 seconds bcuz calling this every think call is overkill
-timer.Create("Eradium.MouthMove.Timer",0.01, 0,function()
-	if speaking then
-		updateSpeak(true)
-	end
-end)
+	-- removing them to a list of speaking players
+	Era_hAdd("PlayerEndVoice", function(ply)
+		updateSpeak(false)
+		speaking = nil
+	end)
+
+	-- Create a timer clientside to update the server on our voice volume
+	-- we also call this every .01 seconds bcuz calling this every think call is overkill
+	timer.Create("Eradium.MouthMove.Timer", 0.01, 0, function()
+		if speaking then
+			updateSpeak(true)
+		end
+	end)
+	print("era client side loaded")
+end
+
+-- we run start these here because LocalPlayer is valid at this point
+Era_hAdd("InitPostEntity", Era_LoadCL)
+
+-- if not Era_loaded then
+	-- Era_LoadCL()
+-- end
